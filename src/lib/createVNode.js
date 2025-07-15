dbc5c5a5cbb8558a6f9aa41aa82038d0fef7ed45
q1Lim <cochlea00@gmail.com>
2025-07-16 00:47:14 +0900

    feat: createVNode 작성

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,7 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type: type,
+    props: props,
+    children: children.flat(Infinity).filter((elem) => elem !== false && elem !== null && elem !== undefined),
+  };
 }
