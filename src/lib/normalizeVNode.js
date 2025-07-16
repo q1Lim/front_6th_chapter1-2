5e30fcf4a77b4b7d44c9ae270a9b699049735bbb
q1Lim <cochlea00@gmail.com>
2025-07-17 01:47:26 +0900

    feat: normalizeVNode 작성

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,26 @@
 export function normalizeVNode(vNode) {
+  // null, undefined, boolean 값은 빈 문자열로 변환되어야 한다.
+  if (vNode === null || vNode === undefined || vNode === true || vNode === false) {
+    return '';
+  }
+  // 문자열과 숫자는 문자열로 변환되어야 한다.
+  if (typeof vNode === 'string') {
+    return vNode;
+  }
+  if (typeof vNode === 'number') {
+    return String(vNode);
+  }
+  // 컴포넌트를 정규화한다.
+  // 함수형 컴포넌트를 실행해서 normalizeVNode로 재귀적으로 처리
+  if (typeof vNode.type === 'function') {
+    return normalizeVNode(vNode.type({ ...vNode.props, children: vNode.children }));
+  }
+  // 자식 노드에서 제거
+  if (Array.isArray(vNode.children)) {
+    return {
+      ...vNode,
+      children: vNode.children.map((child) => normalizeVNode(child)).filter((child) => child !== ''),
+    };
+  }
   return vNode;
 }
